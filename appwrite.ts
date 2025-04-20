c674578686f054d6f242a9580ed45571c8bf157a
mohamed-h-rabie <mohamed.hassa3nnn@gmail.com>
2025-04-20 05:57:25 +0200

    hide statusBar

---
+++ appwrite.ts
@@ -0,0 +1,6 @@
+import { Client, Account } from "react-native-appwrite";
+
+const client = new Client()
+  .setEndpoint("https://fra.cloud.appwrite.io/v1")
+  .setProject("68039a91002dc427df55")
+  .setPlatform("MovieFlix");
